03292feeff1ff5fbccb964ffa843bad4e9b5115a
Kuumoneko <maingocnhat2007@gmail.com>
2025-11-07 08:37:15 +0700

    a

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+    outputFileTracingIncludes: {
+        '/*': ['./node_modules/youtube-po-token-generator/vendor/**'],
+    },
+}
 
 export default nextConfig;

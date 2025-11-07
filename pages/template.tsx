da93391570cf6553f798541764570714b978999b
Kuumoneko <maingocnhat2007@gmail.com>
2025-11-07 09:38:37 +0700

    a

--- pages/template.tsx
+++ pages/template.tsx
@@ -6,11 +6,13 @@ import { useEffect, useState } from "react";
 export default function Template({ children }: { children: React.ReactNode }) {
     const [url, seturl] = useState("/");
     useEffect(() => {
+        alert("This web can not play audio currently. I will fix it later.")
         const run = setInterval(() => {
             seturl(localStorage.getItem("url") ?? "/");
         }, 100);
         return () => clearInterval(run);
     }, []);
+
     return (
         <div className="w-screen h-screen flex flex-col items-center justify-center bg-slate-900 cursor-default select-none">
             <Nav seturl={seturl} />

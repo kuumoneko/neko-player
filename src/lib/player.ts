7ffe1d3b58a2baf5975bfd809430095229bb2eff
Kuumoneko <maingocnhat2007@gmail.com>
2025-11-07 09:30:42 +0700

    a

--- src/lib/player.ts
+++ src/lib/player.ts
@@ -1,7 +1,7 @@
 import { Api_key, Track } from "@/types";
 import Spotify from "./player/spotify";
 import Youtube from "./player/youtube";
-import Innertube, { ClientType } from "youtubei.js/web.bundle";
+import Innertube, { ClientType } from "youtubei.js/web";
 import * as ytb from "youtubei.js"
 
 

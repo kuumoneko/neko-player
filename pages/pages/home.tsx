a0f7b84e8b9ddaa1e4b86ecdc30bdace23d13ef4
Kuumoneko <maingocnhat2007@gmail.com>
2025-11-12 00:01:10 +0700

    update home Page

--- pages/pages/home.tsx
+++ pages/pages/home.tsx
@@ -1,7 +1,7 @@
 import { Artist, Playlist, Track } from "@/types";
 import home_page from "@/utils/music/home";
 import { useEffect, useState } from "react";
-import formatDuration from "../../src/utils/format";
+import { formatDuration, remove_hashtag } from "@/utils/format";
 import { goto } from "@/lib/url";
 import Play from "@/components/Show/common/play";
 
@@ -18,7 +18,20 @@ export default function HomePage({
             const temp = await home_page();
             setartists(temp.artist);
             setplaylists(temp.playlist);
-            setnew_tracks(temp.new_tracks);
+            const tempp = temp.new_tracks.sort((a: Track, b: Track) => {
+                const [yearA, monthA, dayA] = a.releasedDate
+                    .split("-")
+                    .map(Number);
+                const [yearB, monthB, dayB] = b.releasedDate
+                    .split("-")
+                    .map(Number);
+
+                const dateA = new Date(yearA, monthA - 1, dayA);
+                const dateB = new Date(yearB, monthB - 1, dayB);
+
+                return dateB.getTime() - dateA.getTime();
+            });
+            setnew_tracks(tempp);
         }
         run();
     }, []);
@@ -30,12 +43,18 @@ export default function HomePage({
                     <div className="flex flex-row overflow-x-scroll [&::-webkit-scrollbar]:hidden">
                         {artists.map((artist: Artist) => {
                             return (
-                                <div className="flex flex-row items-center mr-4 my-3 bg-slate-600 p-2 rounded-4xl hover:bg-slate-500 hover:cursor-pointer" onClick={() => {
-                                    goto(`/artists/${artist.source}/${artist.id}` , seturl);
-                                }}>
+                                <div
+                                    className="flex flex-row items-center mr-4 my-3 bg-slate-600 p-2 rounded-4xl hover:bg-slate-500 hover:cursor-pointer"
+                                    onClick={() => {
+                                        goto(
+                                            `/artists/${artist.source}/${artist.id}`,
+                                            seturl
+                                        );
+                                    }}
+                                >
                                     <div>
                                         <img
-                                        className="mr-2 rounded-2xl"
+                                            className="mr-2 rounded-2xl"
                                             src={artist.thumbnail}
                                             height={50}
                                             width={50}
@@ -54,11 +73,30 @@ export default function HomePage({
                     <div className="flex flex-row items-center">
                         {playlists.map((playlist: Playlist) => {
                             return (
-                                <div className="flex flex-row items-center mr-4 my-3 bg-slate-600 p-2 rounded-4xl hover:bg-slate-500 hover:cursor-pointer" onClick={() => {
-                                    goto(`/playlists/${playlist.source}/${playlist.id}` , seturl);
-                                }}>
-                                    <img className="mr-2 rounded-2xl" src={playlist.thumbnail} height={playlist.source === "spotify" ? 60 : 50} width={playlist.source === "spotify" ? 60 : 80} />
-                                    <div>{playlist.name}</div>
+                                <div
+                                    className="flex flex-row items-center mr-4 my-3 bg-slate-600 p-2 rounded-4xl hover:bg-slate-500 hover:cursor-pointer"
+                                    onClick={() => {
+                                        goto(
+                                            `/playlists/${playlist.source}/${playlist.id}`,
+                                            seturl
+                                        );
+                                    }}
+                                >
+                                    <img
+                                        className="mr-2 rounded-2xl"
+                                        src={playlist.thumbnail}
+                                        height={
+                                            playlist.source === "spotify"
+                                                ? 60
+                                                : 50
+                                        }
+                                        width={
+                                            playlist.source === "spotify"
+                                                ? 60
+                                                : 80
+                                        }
+                                    />
+                                    <div>{playlist.name.slice(0, 25)}</div>
                                 </div>
                             );
                         })}
@@ -69,15 +107,29 @@ export default function HomePage({
                 <div className="grid grid-cols-3 gap-4 overflow-y-scroll [&::-webkit-scrollbar]:hidden">
                     {new_tracks.map((track: Track) => {
                         return (
-                            <div className="flex flex-row items-center rounded-2xl hover:cursor-pointer hover:bg-slate-600" 
-                            onDoubleClick={() => {
-                                Play(track, track.source, "track", track.id, [track])
-                            }}>
-                                <img className="mr-2 rounded-2xl" src={track.thumbnail} alt="" />
+                            <div
+                                className="flex flex-row items-center rounded-2xl hover:cursor-pointer hover:bg-slate-600"
+                                onClick={() => {
+                                    Play(
+                                        track,
+                                        track.source,
+                                        "track",
+                                        track.id,
+                                        [track]
+                                    );
+                                }}
+                            >
+                                <img
+                                    className="mr-2 rounded-2xl"
+                                    src={track.thumbnail}
+                                    alt=""
+                                />
                                 <div className="flex flex-col">
-                                    <div>{track.name}</div>
+                                    <div>{remove_hashtag(track.name)}</div>
                                     <div className="flex flex-row">
-                                        <div className="mr-4">{track.artist[0].name}</div>
+                                        <div className="mr-4">
+                                            {track.artist[0].name}
+                                        </div>
                                         <div>
                                             {formatDuration(
                                                 track.duration / 1000
